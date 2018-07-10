8ded3c03bcbc5b1dc8cdca28689cd803eb604caa
AHowardC <achsvgusaeng@yahoo.com>
2018-07-09 20:08:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send({hi: 'there'});
+});
+// make port constant to dynamically bind based on heroku port assignment or port 5000 on local
+const PORT = process.env.PORT||5000;
+app.listen(PORT);
+
+// localhost:5000
