3cd1c42f7f7600a9e3ebbdbc5b26ea3657e76b9e
AHowardC <achsvgusaeng@yahoo.com>
2018-07-09 20:34:56 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send({hi: 'there'});
+    res.send({Bob: 'the man'});
 });
 // make port constant to dynamically bind based on heroku port assignment or port 5000 on local
 const PORT = process.env.PORT||5000;
